d7fc1110e5d686c995b350923c5cb6c175f4b8d6
KasparsMazurs <kaspars.mazurs@gmail.com>
2022-10-02 18:11:27 +0000

    define all functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,66 @@ class AudioController {
         this.victorySound.play();
     }
 }
+
+class MixOrMatch {
+
+// Sets everything for game
+constructor(totalTime, cards) {
+
+}
+
+// Stars game
+startGame() {
+
+}
+
+// Starts to count time  
+startTimer() {
+
+}
+
+//Victory text overlay visible
+victory() {
+
+}
+
+// Hide cards after seeing them
+hideCards() {
+
+}
+
+// Flip cards when clicked
+flipCard(card) {
+
+}
+
+// check if cards mach
+checkForCardMatch(card) {
+    
+}
+
+// If cards match then they do not flip
+cardMatch(card1, card2) {
+
+}
+
+// If cards are mismatch then flip them both
+cardMismatch(card1, card2) {
+
+}
+
+// Shuffle cards
+shuffleCards(cardsArray) {
+
+}
+
+// Get cards
+getCardType(card) {
+
+}
+
+// Flip cards
+canFlipCard(card) {
+
+}
+}
