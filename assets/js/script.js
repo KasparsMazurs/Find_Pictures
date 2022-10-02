267145d36c24de60afd85b18185475eefb50dadb
KasparsMazurs <kaspars.mazurs@gmail.com>
2022-10-02 12:03:39 +0000

    add some music and sound in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,13 @@ function ready() {
         });
     });
 }
+
+class AurioC {
+    constructor() {
+        this.music = new Audio('../Sounds/jungle-kids-116652.mp3')
+        this.flip = new Audio('../Sounds/flipcard-91468.mp3')
+        this.success = new Audio('../Sounds/success-1-6297.mp3')
+        this.victory = new Audio('../Sounds/success-fanfare-trumpets-6185.mp3')
+    }
+}
+let audioC = new AudioC();
